6857797f11d5abce1578bbd288813e9bf62c9a48
Jagadeesh1961 <108126056+Jagadeesh1961@users.noreply.github.com>
2024-05-05 10:18:30 +0530

    Update index.js

--- src/components/TaskItem/index.js
+++ src/components/TaskItem/index.js
@@ -1,29 +1,49 @@
 import { MdDelete } from "react-icons/md";
 
-import "./index.css"
+import "./index.css";
 
 const TaskItem = (props) => {
-    const {taskItem,toggleCheckItem,deleteTodoItem} = props;
-    const {isChecked,id, text}  = taskItem;
-    const labelClassName = isChecked ? "checkbox-label checked" : "checkbox-label"
+    const { taskItem, toggleCheckItem, deleteTodoItem } = props;
 
-    const onClickCheckItem = ()=>{
-        toggleCheckItem(id)
+    // Destructure properties from taskItem
+    const { isChecked, id, text } = taskItem;
+
+    // Determine the class name for the label based on isChecked
+    const labelClassName = isChecked ? "checkbox-label checked" : "checkbox-label";
+
+    // Function to handle clicking on the checkbox
+    const onClickCheckItem = () => {
+        toggleCheckItem(id);
     }
+
+    // Function to handle deleting a todo item
     const onDeleteTodo = () => {
-        deleteTodoItem(id)
+        deleteTodoItem(id);
     }
+
+    // Render each task item
     return (
         <li className="todo-item-container">
-            <input className="checkbox-input" type="checkbox" id = {id} onClick={onClickCheckItem} checked={isChecked}/>
+            {/* Checkbox input */}
+            <input 
+                className="checkbox-input" 
+                type="checkbox" 
+                id={id} 
+                onClick={onClickCheckItem} 
+                checked={isChecked} 
+            />
+            {/* Task label */}
             <div className="label-container">
-                <label className={labelClassName} htmlFor={id}>{text}</label>
+                <label className={labelClassName} htmlFor={id}>
+                    {text}
+                </label>
+                {/* Delete icon */}
                 <div className="delete-icon-container">
-                    <MdDelete className="delete-icon" onClick={onDeleteTodo}/>
+                    <MdDelete className="delete-icon" onClick={onDeleteTodo} />
                 </div>
             </div>
         </li>
-    )
+    );
 }
 
-export default TaskItem
+export default TaskItem;
